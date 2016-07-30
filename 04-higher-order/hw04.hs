88b873eb2fa09de2306000c4e5b4fe303a1bf84e
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2016-07-30 09:20:14 -0700

    hw4: ex1 and 2 solved

--- 04-higher-order/hw04.hs
+++ 04-higher-order/hw04.hs
@@ -3,43 +3,6 @@
 module Homework where
 
 import Data.List
--------------------------------------------------------------------------------
--- Exercise 4
-
--- Return all odd prime numbers up to 2 * @n@ + 2
-sieveSundaram :: Integer -> [Integer]
-sieveSundaram n = map ((+1) . (*2)) $ [1..n] \\ sieve
-  where sieve = map (\(i, j) -> i + j + 2*i*j)
-                . filter (\(i, j) -> i + j + 2*i*j <= n)
-                $ cartProd [1..n] [1..n]
-
--- Return all possible pairs
-cartProd :: [a] -> [b] -> [(a, b)]
-cartProd xs ys = [(x,y) | x <- xs, y <- ys]
-
--------------------------------------------------------------------------------
--- Exercise 3
-
--- Returns True if and only if there are an odd number of True values contained
--- in the input line. It does not matter how many False values the input list
--- contains.
--- > xor [False, True, False] == True
--- > xor [False, True, False, False, True] == False
-xor :: [Bool] -> Bool
-xor = foldr xor2 False
-
-xor2 :: Bool -> Bool -> Bool
-xor2 a b = (a || b) && not (a && b)
-
--- map as a fold
-map' :: (a -> b) -> [a] -> [b]
-map' f = foldr (\x y -> f x : y) []
-
--- foldl with foldr
-myFoldl :: (a -> b -> a) -> a -> [b] -> a
-myFoldl f x = foldr (flip f) x . reverse
-
--------------------------------------------------------------------------------
 -- Exercise 2
 
 data Tree a = Leaf
