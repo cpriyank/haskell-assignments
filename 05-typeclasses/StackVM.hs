9eda245a3f0947d713dedaf1e4d8b31e24a558f8
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2016-08-02 20:43:54 -0700

    stackvm complete and tested

--- 05-typeclasses/StackVM.hs
+++ 05-typeclasses/StackVM.hs
@@ -35,3 +35,24 @@ execute (s:_) []                            = Right s
 execute s (PushI x : xs)                    = execute (IVal x : s) xs
 execute s (PushB x : xs)                    = execute (BVal x : s) xs
 
+execute (IVal s1 : IVal s2 : ss) (Add : xs) = execute (s':ss) xs
+    where s' = IVal (s1 + s2)
+execute (_:_:_) (Add:_)                     = errType "Add"
+execute _ (Add:_)                           = errUnderflow "Add"
+
+execute (IVal s1:IVal s2:ss) (Mul : xs)     = execute (s':ss) xs
+    where s' = IVal (s1 * s2)
+execute (_:_:_) (Mul:_)                     = errType "Mul"
+execute _ (Mul:_)                           = errUnderflow "Mul"
+
+execute (BVal s1:BVal s2:ss) (And : xs)     = execute (s':ss) xs
+    where s' = BVal (s1 && s2)
+execute (_:_:_) (And:_)                     = errType "And"
+execute _ (And:_)                           = errUnderflow "And"
+
+execute (BVal s1 : BVal s2 : ss) (Or : xs)  = execute (s':ss) xs
+    where s' = BVal (s1 || s2)
+execute (_:_:_) (Or:_)                      = errType "Or"
+execute _ (Or:_)                            = errUnderflow "Or"
+
+test = stackVM [PushI 3, PushI 5, Add]
