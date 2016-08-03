903ff96e3f152255b8471d42b21e4906adb9a55a
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2016-08-03 16:56:49 -0700

    hw5.4 finally working

--- 05-typeclasses/Calc.hs
+++ 05-typeclasses/Calc.hs
@@ -44,3 +44,50 @@ instance Expr ExprT where
 reify :: ExprT -> ExprT
 reify = id
 
+-------------------------------------------------------------------------------
+-- Exercise 4
+
+-- Should work like original calculator
+instance Expr Integer where
+  lit = id  -- if this is confusing look at the type signatur of the typeclass
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit x
+    | x <= 0    = False
+    | otherwise = True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y)= MinMax (max x y)
+  mul (MinMax x) (MinMax y)= MinMax (min x y)
+
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y)= Mod7 ((x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y)= Mod7 ((x * y) `mod` 7)
+
+-- tests
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger :: Maybe Integer
+testInteger = testExp
+
+testBool :: Maybe Bool
+testBool = testExp
+
+testMM :: Maybe MinMax
+testMM = testExp
+
+testSat :: Maybe Mod7
+testSat = testExp
+
