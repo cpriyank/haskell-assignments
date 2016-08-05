ee9dbd2726016233b4c054d7b4c30cf87961a136
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2016-08-05 10:22:49 -0700

    simple things first

---
+++ 06-lazy/Fibonacci.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+
+{-# LANGUAGE FlexibleInstances #-}
+
+module Fibonacci where
+
+--------------------------------------------------------------------------------
+-- Exercise 1
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib(n-1) + fib (n-2)
+
+fibs1 :: [Integer]
+fibs1 = fmap fib [0..]
+
