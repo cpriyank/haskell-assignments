d743668cd82169910d4124b14d53691433fafe0e
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2016-08-12 21:39:55 -0700

    IO hw finished with tests

--- 08-io/Party.hs
+++ 08-io/Party.hs
@@ -28,3 +28,29 @@ glCons emp@(Emp {empFun = ef}) (GL lst gf) = GL (emp:lst) (ef+gf)
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun a b = if a > b then a else b
 
+{-treeFold :: (b -> a -> b) -> b -> Tree a -> b-}
+{-treeFold f init (Node {rootLabel = rl, subForest = sf})-}
+  {-= f (foldl (treeFold f) init sf) rl-}
+
+treeFold :: (a -> [b] -> b) -> b -> Tree a -> b
+treeFold f init (Node {rootLabel = rl, subForest = sf})
+  = f rl (map (treeFold f init) sf)
+
+-- | First part of list is with boss.
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss bestLists = (maximumS withBossL, maximumS withoutBossL)
+  where withoutBossL   = map fst bestLists
+        -- ^ The new withoutBossList has sub bosses in it.
+
+        withoutSubBoss = map snd bestLists
+        withBossL      = map (glCons boss) withoutSubBoss
+        -- ^ The new withBossList doesn't have sub bosses in it.
+
+maximumS ::(Monoid a, Ord a) => [a] -> a
+maximumS [] = mempty
+maximumS lst = maximum lst
+
+maxFun :: Tree Employee -> GuestList
+maxFun tree = uncurry max res
+  where res = treeFold nextLevel (mempty, mempty) tree
+
