599273c6125dead4faba1b2ea693bcaf60ffb83c
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2016-08-15 17:32:56 -0700

    finishing up hw11

--- 11-applicative-functors-part2/SExpr.hs
+++ 11-applicative-functors-part2/SExpr.hs
@@ -31,3 +31,31 @@ spaces = zeroOrMore (satisfy isSpace)
 ident :: Parser String
 ident = liftA2 (:) (satisfy isAlpha) (zeroOrMore $ satisfy isAlphaNum)
 
+------------------------------------------------------------
+--  3. Parsing S-expressions
+------------------------------------------------------------
+
+-- An "identifier" is represented as just a String; however, only
+-- those Strings consisting of a letter followed by any number of
+-- letters and digits are valid identifiers.
+type Ident = String
+
+-- An "atom" is either an integer value or an identifier.
+data Atom = N Integer | I Ident
+  deriving Show
+
+-- An S-expression is either an atom, or a list of S-expressions.
+data SExpr = A Atom
+           | Comb [SExpr]
+  deriving Show
+
+parseSExpr :: Parser SExpr
+parseSExpr = spaces *>
+              ( (A <$> parseAtom)
+              <|>
+              (char '(' *> (Comb <$> oneOrMore parseSExpr) <* char ')') )
+             <* spaces
+
+parseAtom :: Parser Atom
+parseAtom = N <$> posInt <|> I <$> ident
+
