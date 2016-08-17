ce6282ecd7d92d0d0dd1075eaafb3541cc8ba580
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2016-08-17 10:04:46 -0700

    some progress made

---
+++ 12-monads/Risk.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Risk where
+
+import Control.Applicative
+import Control.Monad.Random
+import Control.Monad
+import Data.List (sort)
+
+------------------------------------------------------------
+-- Die values
+
+newtype DieValue = DV { unDV :: Int }
+  deriving (Eq, Ord, Show, Num)
+
+first :: (a -> b) -> (a, c) -> (b, c)
+first f (a, c) = (f a, c)
+
+instance Random DieValue where
+  random           = first DV . randomR (1,6)
+  randomR (low,hi) = first DV . randomR (max 1 (unDV low), min 6 (unDV hi))
+
+die :: Rand StdGen DieValue
+die = getRandom
+
+dice :: Int -> Rand StdGen [DieValue]
+dice n = replicateM n die
+
