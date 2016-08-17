ad9e659fb1ab17f5ed730eb1e0c638bb67f49da1
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2016-08-17 16:45:50 -0700

    success function working

--- 12-monads/Risk.hs
+++ 12-monads/Risk.hs
@@ -26,3 +26,24 @@ die = getRandom
 dice :: Int -> Rand StdGen [DieValue]
 dice n = replicateM n die
 
+------------------------------------------------------------
+-- Risk
+
+type Army = Int
+
+data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+  deriving Show
+
+exactSuccessProb :: Battlefield -> Double
+exactSuccessProb bf = 0
+
+-- | Runs 1000 invades and uses the results to compute a @Double@ between 0 and
+-- 1 representing the estimated probability that the attacking army will
+-- completely destroy the defending army.
+succesProb :: Battlefield -> Rand StdGen Double
+succesProb bf = replicateM 1000 (invade bf) >>= success
+
+success :: [Battlefield] -> Rand StdGen Double
+success bfs = return $ fromIntegral (length x)  / fromIntegral (length bfs)
+  where x = filter ((== 0) . defenders) bfs
+
